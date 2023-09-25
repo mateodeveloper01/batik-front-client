b1fb1c1da351747830376a93ccd80c98b4897dcf
mateopedehonta <mateopedehonta@gmail.com>
2023-09-25 14:25:33 -0300

    cambio

--- src/pages/acount/register/index.tsx
+++ src/pages/acount/register/index.tsx
@@ -11,7 +11,11 @@ import {
   Input,
   FormLabel,
   FormHelperText,
+  Alert,
+  AlertIcon,
+  AlertTitle,
 } from "@chakra-ui/react";
+import { useState } from "react";
 const schema = z.object({
   username: z.string().min(6),
   email: z.string().email("Email invalido"),
@@ -21,6 +25,8 @@ const schema = z.object({
 type fieldValues = z.infer<typeof schema>;
 
 const Register = () => {
+  const [alert, setAlert] = useState<"success" | "error" | null>(null);
+
   const {
     register,
     handleSubmit,
@@ -32,18 +38,17 @@ const Register = () => {
   const onSubmit = (data: any) => {
     console.log(data);
     axios
-      .post(
-        `${env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/auth/register`,
-        data,
-        {
-          headers: {
-            Authorization: "bearer " + env.NEXT_PUBLIC_API_TOKEN,
-          },
-        }
-      )
-      .then((data) => console.log(data));
+      .post(`${env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/auth/register`, data, {
+        headers: {
+          Authorization: "bearer " + env.NEXT_PUBLIC_API_TOKEN,
+        },
+      })
+      .then((data) => {
+        setAlert("success");
+      })
+      .catch(() => setAlert("error"));
   };
-  const onError = (error: any) => console.log({ error });
+  const onError = (error: any) => setAlert("error");
 
   return (
     <div className="flex min-h-[50vh] w-full flex-col items-center justify-center gap-5">
@@ -52,25 +57,6 @@ const Register = () => {
         onSubmit={handleSubmit(onSubmit, onError)}
         className="flex  w-1/4 flex-col items-center justify-center gap-5 [&>*]:w-full "
       >
-        {/* <TextField
-          helperText={errors.password?.message}
-          error={errors.password ? true : false}
-          label="username"
-          {...register("username")}
-        />
-        <TextField
-          helperText={errors.email?.message}
-          error={errors.email ? true : false}
-          label="Email"
-          {...register("email")}
-        />
-        <TextField
-          helperText={errors.password?.message}
-          error={errors.password ? true : false}
-          label="Password"
-          //   type="password"
-          {...register("password")}
-        /> */}
         <FormControl>
           <FormLabel>Email</FormLabel>
           <Input type="email" {...register("email")} />
@@ -92,9 +78,21 @@ const Register = () => {
         <Button variant="link">
           <Link href={"/acount/login"}>
             Ya estas registrado?
-            <br /><label className="text-[#319795]">Inicia sesion aqui</label>
+            <br />
+            <label className="text-[#319795]">Inicia sesion aqui</label>
           </Link>
         </Button>
+        {alert ? (
+          <Alert status={alert} mt={4}>
+            <AlertIcon />
+            {alert === "error" && (
+              <AlertTitle>Email o contraseña Incorrecto</AlertTitle>
+            )}
+            {alert === "success" && <AlertTitle>Usuario Registrado</AlertTitle>}
+          </Alert>
+        ) : (
+          <></>
+        )}
         <DevTool control={control} />
       </form>
     </div>
