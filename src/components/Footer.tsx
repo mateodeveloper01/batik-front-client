0a83ea04644cd312bebe5ac151cb3984f43bde05
mateopedehonta <mateopedehonta@gmail.com>
2023-10-13 15:38:39 -0300

    cambio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,22 +11,23 @@ const Footer = () => {
     queryFn: () => getItem("/categories"),
   });
   return (
-    <div className="px-10 pb-20 pt-32 max-md:px-2 max-md:pt-4">
-      <div className="flex h-max gap-5 max-md:flex-col">
-        <div className=" flex w-1/3  gap-5 max-md:w-1/2">
-          <div className="flex  flex-col max-md:w-full max-md:p-4">
-            <h1>Categorias</h1>
-            {isLoading ? (
-              <></>
-            ) : (
-              data.map((item) => (
-                <Link href={`/products/${item.title}`}>
-                  <Text textTransform="capitalize">{item.title}</Text>
-                </Link>
-              ))
-            )}
-          </div>
-          {/* <div className=" flex  flex-col max-md:w-full">
+    <div className="flex justify-center">
+      <div className="w-screen max-w-[1310px] px-10 pb-20 pt-32 max-md:px-2 max-md:pt-4">
+        <div className="flex h-max gap-5 max-md:flex-col">
+          <div className=" flex w-1/3  gap-5 max-md:w-1/2">
+            <div className="flex  flex-col max-md:w-full max-md:p-4">
+              <h1>Categorias</h1>
+              {isLoading ? (
+                <></>
+              ) : (
+                data.map((item) => (
+                  <Link href={`/products/${item.title}`}>
+                    <Text textTransform="capitalize">{item.title}</Text>
+                  </Link>
+                ))
+              )}
+            </div>
+            {/* <div className=" flex  flex-col max-md:w-full">
             <h1>Links</h1>
             <span>FAQ</span>
             <span>Pages</span>
@@ -34,40 +35,41 @@ const Footer = () => {
             <span>compare</span>
             <span>Cookies</span>
           </div> */}
-        </div>
-        <div className="flex w-2/3 justify-center  gap-5 max-md:w-full max-md:p-4">
-          <div className="flex h-32 flex-col ">
-            <h1>Nosotros</h1>
-            <p className="">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. A facere
-              porro minus placeat libero vel ducimus at ipsum esse? Enim,
-              laborum officiis voluptate at similique obcaecati quia ex.
-              Voluptas, labore!
-            </p>
           </div>
-          <div className="flex flex-col ">
-            <h1>Contacto</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero
-              atque ullam modi quo eos tempora. Quia voluptate, totam laudantium
-              quam voluptates vel sequi asperiores cupiditate at id minima
-              provident.
-            </p>
+          <div className="flex w-2/3 justify-center  gap-5 max-md:w-full max-md:p-4">
+            <div className="flex h-32 flex-col ">
+              <h1>Nosotros</h1>
+              <p className="">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. A
+                facere porro minus placeat libero vel ducimus at ipsum esse?
+                Enim, laborum officiis voluptate at similique obcaecati quia ex.
+                Voluptas, labore!
+              </p>
+            </div>
+            <div className="flex flex-col ">
+              <h1>Contacto</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+                libero atque ullam modi quo eos tempora. Quia voluptate, totam
+                laudantium quam voluptates vel sequi asperiores cupiditate at id
+                minima provident.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex items-center justify-between pt-10 max-md:flex-col-reverse ">
-        <div className="flex items-center gap-4 ">
-          <h1 className="text-blue-700">BATIK</h1>
-          <span>© Copyright 2023. All Rights Reserved</span>
-        </div>
-        <div className="flex justify-end max-md:pb-10 max-md:justify-center items-center">
-          {/* <img className="w-4/5" src="img/payment.png" alt="" /> */}
-          <div className="max-md:hidden">
-            <ListCreditCard w={9} h={7} />
+        <div className="flex items-center justify-between pt-10 max-md:flex-col-reverse ">
+          <div className="flex items-center gap-4 ">
+            <h1 className="text-blue-700">BATIK</h1>
+            <span>© Copyright 2023. All Rights Reserved</span>
           </div>
-          <div className="md:hidden">
-            <ListCreditCard w={5} h={4} />
+          <div className="flex items-center justify-end max-md:justify-center max-md:pb-10">
+            {/* <img className="w-4/5" src="img/payment.png" alt="" /> */}
+            <div className="max-md:hidden">
+              <ListCreditCard w={9} h={7} />
+            </div>
+            <div className="md:hidden">
+              <ListCreditCard w={5} h={4} />
+            </div>
           </div>
         </div>
       </div>
